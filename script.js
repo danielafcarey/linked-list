5ce5435b569b17d9c4a9164d2346d11572c4c25c
Daniela Carey <danielafcarey@gmail.com>
2018-02-04 12:34:50 -0700

    Validate bookmark add form
    Delete button removes bookmark card
    Enter button disabled if no contents in input fields
    Known bug to fix: read button only works on every other bookmark card starting from bottom

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 
 //create bookmark section
 var titleInput = document.querySelector('#title-input');
@@ -9,50 +10,57 @@ var bookmarkList = document.querySelector('#bookmark-list');
 var bookmarkCard = document.querySelector('.bookmark-card');
 var cardTitle = document.querySelector('.card-title');
 var cardUrl = document.querySelector('.card-url');
-var readButton;
-var deleteButton = document.querySelector('.delete-button');
-
 
 //event listeners
-createBookmarkButton.addEventListener('click', addBookmarkCard);
+titleInput.addEventListener('input', enableSubmitButton);
+urlInput.addEventListener('input', enableSubmitButton);
+createBookmarkButton.addEventListener('click', validateInput);
 
-var submitCount = 0;
-var readCount = 0;
-var addClass = 0;
-
-function increaseSubmitCount() {
-  readButton = document.querySelectorAll('.read-button');
-  console.log(readButton);
-
-  for (i = 0; i < readButton.length; i++){
-    if (submitCount > 0) {
-      readButton[i].addEventListener('click', markAsRead);
-    }
+function enableSubmitButton() {
+  console.log('work?');
+  if (titleInput.value !== '' || urlInput.value !== '' ) {
+    createBookmarkButton.disabled = false;
+  } else {
+    createBookmarkButton.disabled = true;  
   }
-
 }
 
 //functions
+function validateInput() {
+  console.log('hello');
+
+  if (titleInput.value === '') {
+    alert('You must enter a website title');
+  } else if (urlInput.value === '') {
+    alert('You must enter a URL');  
+  } else {
+  addBookmarkCard();
+  }
+}
+
+
 function addBookmarkCard() {
   var titleInputValue = titleInput.value;
   var urlInputValue = urlInput.value;
 
-  var newBookmarkCard = '<article class="bookmark-card"><h2 class="card-title article-' + addClass + '">' + titleInputValue + '</h2><hr /><a class="card-url" href="#">' + urlInputValue + '</a><hr /><button class="read-button" type="button">Read</button><button class="delete-button" type="button">Delete</button></article>';
+  var newBookmarkCard = '<article class="bookmark-card"><h2 class="card-title article-">' + titleInputValue + '</h2><hr /><a class="card-url" href="#">' + urlInputValue + '</a><hr /><button class="read-button" type="button">Read</button><button class="delete-button" type="button">Delete</button></article>';
 
-  addClass++;
-
-  console.log(titleInputValue);
-  console.log(urlInputValue);
   bookmarkList.insertAdjacentHTML('beforeend', newBookmarkCard); 
-
-  submitCount++;
-  increaseSubmitCount(); 
+  markAsRead();
+  deleteCard();
 }
 
 function markAsRead() {
-  readButton.className = 'read';
+  $('.read-button').on('click', function() {
+      $(this).closest('.bookmark-card').toggleClass('read');
+      console.log('WORK!');
+  });
 }
 
-function markAsUnread() {
-
+function deleteCard() {
+  $('.delete-button').on('click', function(){
+      $(this).closest('.bookmark-card').remove();
+  });
 }
+
+});
